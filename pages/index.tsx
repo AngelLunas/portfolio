1f1491f12896378f04526d71c27a765c9dee83d6
Angel David Luna Ospina <angel.luna@utp.edu.co>
2025-12-31 11:08:52 -0500

    Fix: Correct component import paths - use capitalized Components directory

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
 import styles from '../styles/Home.module.css'
-import Main from '../components/Main'
-import About from '../components/About'
-import Contact from '../components/Contact'
-import Footer from '../components/Footer'
-import WrapperContext from '../components/Context';
+import Main from '../Components/Main'
+import About from '../Components/About'
+import Contact from '../Components/Contact'
+import Footer from '../Components/Footer'
+import WrapperContext from '../Components/Context';
 const Projects = dynamic(() => import('../components/Projects'), { ssr: false });
 const Header = dynamic(() => import('../components/Header'), { ssr: false });
 
